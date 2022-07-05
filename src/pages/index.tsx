6c8187e6417fed1667a9cf86b04d70227d670d21
techpril <priscila.teodorio2015@gmail.com>
2022-07-05 18:31:55 -0300

    Add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+
+
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-      <h1>Oi imundo</h1>
+      <h1 className={styles.title}>
+        Oi imundo
+      </h1>
   )
 }
